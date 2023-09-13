09b5310b04f07d3d42da4f4fd04b42dc2b1c555e
Paul Heading <paulheading@me.com>
2023-09-13 13:01:48 +0100

    images config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  image: {
+    domains: ["https://i.scdn.co"],
+  },
   scopedStyleStrategy: "class",
   integrations: [mdx()],
 });
