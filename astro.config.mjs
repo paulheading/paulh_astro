271c081a643a4bf93beccf3f44f84c865cdc4f32
Paul Heading <paulheading@me.com>
2023-09-13 13:04:34 +0100

    try that again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  image: {
-    domains: ["https://i.scdn.co"],
+  images: {
+    domains: ["mosaic.scdn.co", "i.scdn.co", "images-ak.spotifycdn.com"],
   },
   scopedStyleStrategy: "class",
   integrations: [mdx()],
