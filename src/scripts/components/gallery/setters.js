36ea645ba8dddbdba059eac541bc8567f7dcd6b2
Paul Heading <paulheading@me.com>
2023-09-25 17:23:49 +0100

    inteligent iframes

--- src/scripts/components/gallery/setters.js
+++ src/scripts/components/gallery/setters.js
@@ -48,10 +48,76 @@ function unmuted() {
   state($.overlay_mute, "unmuted");
 }
 
+function adjustOffset(value, properties) {
+  const styles = getComputedStyle($.overlay);
+
+  properties.forEach(function (property) {
+    property = styles.getPropertyValue(property); // fetch property value
+    property = property.slice(0, -2); // remove px from string
+    property = Number(property); // coerce string value to number
+    value -= property; // deduct property value from result
+  });
+
+  return value;
+}
+
+function resizeIFRAME(player) {
+  const content = {};
+
+  content.height = player.getAttribute("content-height");
+
+  content.width = player.getAttribute("content-width");
+
+  content.ratio = content.width / content.height;
+
+  const properties = {};
+
+  properties.height = [
+    "border-bottom-width",
+    "border-top-width",
+    "padding-bottom",
+    "padding-top",
+  ];
+
+  properties.width = [
+    "border-right-width",
+    "border-left-width",
+    "padding-right",
+    "padding-left",
+  ];
+
+  const overlay = {};
+
+  overlay.height = adjustOffset($.overlay.offsetHeight, properties.height);
+
+  overlay.width = adjustOffset($.overlay.offsetWidth, properties.width);
+
+  overlay.isTallEnough = overlay.height >= content.height;
+
+  overlay.isWideEnough = overlay.width >= content.width;
+
+  if (!overlay.isWideEnough) {
+    player.width = Math.round(overlay.width);
+    player.height = Math.round(overlay.width / content.ratio);
+  } else if (!overlay.isTallEnough) {
+    player.height = Math.round(overlay.height);
+    player.width = Math.round(overlay.height * content.ratio);
+  } else {
+    player.height = content.height;
+    player.width = content.width;
+  }
+
+  return player;
+}
+
 function controls() {
-  const player = get.$player();
+  let player = get.$player();
 
   if (!player) return;
+
+  let { tagName } = player;
+
+  if (tagName == "IFRAME") player = resizeIFRAME(player);
 
   const close = $.overlay_close;
 
@@ -71,9 +137,13 @@ function build(element, data) {
   element.height = height;
   element.width = width;
 
+  element.setAttribute("content-height", height);
+
+  element.setAttribute("content-width", width);
+
   element.setAttribute("data-index", index);
 
-  element.style.border = "1px solid white";
+  element.style.outline = "1px solid white";
   element.style.zIndex = 2;
 
   return element;
