7625fd0558e6dd240b5b4cb8b62e13070da10515
Paul Heading <paulheading@me.com>
2023-10-04 17:38:29 +0100

    wrap gallery functionality

--- src/scripts/components/gallery.js
+++ src/scripts/components/gallery.js
@@ -5,19 +5,25 @@ import overlay from "~scripts/components/gallery/overlay";
 import player from "~scripts/components/gallery/player";
 import toggle from "~scripts/components/gallery/toggle";
 
-for (let index = 0; index < $.thumbnails.length; index++) {
-  const $button = $.thumbnails[index];
-  $button.addEventListener("click", overlay.open);
+function createThumbnails() {
+  if (!$.thumbnails) return;
+
+  for (let index = 0; index < $.thumbnails.length; index++) {
+    const $button = $.thumbnails[index];
+    $button.addEventListener("click", overlay.open);
+  }
+
+  $.overlay_play.addEventListener("click", toggle.play);
+
+  $.overlay_mute.addEventListener("click", toggle.mute);
+
+  $.overlay_next.addEventListener("click", player.next);
+
+  $.overlay_prev.addEventListener("click", player.prev);
+
+  $.overlay_close.addEventListener("click", overlay.close);
+
+  window.addEventListener("resize", set.controls);
 }
 
-$.overlay_play.addEventListener("click", toggle.play);
-
-$.overlay_mute.addEventListener("click", toggle.mute);
-
-$.overlay_next.addEventListener("click", player.next);
-
-$.overlay_prev.addEventListener("click", player.prev);
-
-$.overlay_close.addEventListener("click", overlay.close);
-
-window.addEventListener("resize", set.controls);
+createThumbnails();
