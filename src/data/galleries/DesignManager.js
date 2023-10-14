e2f647a3c9fa52239c83a287bc60e72b403b06b8
Paul Heading <paulheading@me.com>
2023-10-14 10:09:14 +0100

    cinema trailer contexts

--- src/data/galleries/DesignManager.js
+++ src/data/galleries/DesignManager.js
@@ -8,22 +8,22 @@ content.push(
   create.content({
     src: trailer("generations"),
     title: "Generations",
-    info: "Test info content for Generations"
+    info: `This is a cinema trailer for a <a href="https://www.barbican.org.uk/whats-on/2018/series/generations-russian-cinema-of-change">film season at the Barbican</a>. I created the trailer using Adobe After Effects with stock footage, images and purchased audio.`,
   }),
   create.content({
     src: trailer("borders_and_boundaries"),
     title: "Borders and Boundaries",
-    info: "Test info content for Borders and Boundaries"
+    info: `This is a cinema trailer for a <a href="https://www.barbican.org.uk/whats-on/2019/series/borders-and-boundaries">film season at the Barbican</a>. I created the trailer using Adobe After Effects with stock footage, images and purchased audio.`,
   }),
   create.content({
     src: trailer("more_than_human"),
     title: "More than Human",
-    info: "Test info content for More than Human"
+    info: `This is a cinema trailer for a <a href="https://www.barbican.org.uk/whats-on/2019/event/ai-more-than-human">major centre-wide exhibition</a> exploring creative and scientific developments in AI. I created the trailer using Adobe After Effects, adapting original artwork.`,
   }),
   create.content({
     src: trailer("watch_this_space"),
     title: "Watch this Space",
-    info: "Test info content for Watch this Space"
+    info: `This is a cinema trailer for the cinema at the Barbican. I created the trailer using Adobe After Effects, adapting original artwork.`,
   })
 );
 
