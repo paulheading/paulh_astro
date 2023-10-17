f76ee82c98b6e51c90e1ca172a1ef9e6b49b986e
Paul Heading <paulheading@me.com>
2023-10-17 17:29:47 +0100

    improved context for developer jobs

--- src/data/galleries/FrontendDesignerDeveloper.js
+++ src/data/galleries/FrontendDesignerDeveloper.js
@@ -8,16 +8,17 @@ content.push(
   create.content({
     src: trailer("events_project_figma"),
     title: "Prototypes",
-    info: `I created these prototypes in <a href="https://www.figma.com/">Figma</a> for the <a href="/project/organising-spaces-with-graphql">Barbican Events Management</a> project. They are really useful for exploring and explaining a hypothetical user journey around a new interface. With prototypes we can also begin collecting user feedback and start testing assumptions before we build the product.`,
+    info: `I created these prototypes in <a href="https://www.figma.com/">Figma</a> for the Barbican <a href="/project/organising-spaces-with-graphql">Events Management</a> project. They're useful for exploring hypothetical user journeys around new interfaces. With prototypes we can also begin collecting user feedback and start testing our assumptions before we build the product.`,
   }),
   create.content({
     src: trailer("jobs_platform_figma"),
     title: "Wireframes",
-    info: `These <a href="https://www.figma.com/">Figma</a> visuals were created for the <a href="https://work.barbican.org.uk">Barbican Careers Platform</a>. This project aimed to create a more intuitive environment for job applications. The plan was to provide a CMS for both administrators and applications. Applicants could login, make multiple applications and track their progress easily.`,
+    info: `These visuals were created in <a href="https://www.figma.com/">Figma</a> for the Barbican <a href="https://work.barbican.org.uk">Careers Platform</a>. This project aims to create a more intuitive environment for job applicants. By providing a CMS for this process, users can make multiple applications, track their progress and upload examples easily.`,
   }),
   create.content({
     src: trailer("accessibility_provisions_teams"),
     title: "Accessibility demo",
+    info: `This is a recording of my fortnightly demo on Microsoft Teams. In this example, I'm presenting my work around how accessibility provision is shown during the ticket purchase journey at the Barbican.`,
   })
 );
 
