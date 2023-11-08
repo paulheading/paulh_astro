1cc5a929afa4e3a4d60278b49994c9c41bfe6b4d
Paul Heading <paulheading@me.com>
2023-11-08 11:39:49 +0000

    bourne street context

--- src/data/galleries/projects/selling_designer_fabric_with_wordpress.js
+++ src/data/galleries/projects/selling_designer_fabric_with_wordpress.js
@@ -7,8 +7,8 @@ const content = [];
 content.push(
   create.content({
     src: trailer("bourne_street_chrome"),
-    title: "Rebrand & Website build",
-    info: `Website build description`,
+    title: "Brand & Website build",
+    info: `It was interesting to develop this brand over time from scratch. I worked with an artist whose sketches I would scan, trace and edit into digital vector artwork. Leather and fabric can also be very technical products to market, so it was interesting to see how <a href="https://en-gb.wordpress.org/">Wordpress</a>  could adapt to the needs of the business.`,
   })
 );
 
