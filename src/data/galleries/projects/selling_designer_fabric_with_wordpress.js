02e99da1ea0d9fc2de1114b29610bf114cf3add5
Paul Heading <paulheading@me.com>
2023-11-15 22:25:57 +0000

    alanabananas vj set

--- src/data/galleries/projects/selling_designer_fabric_with_wordpress.js
+++ src/data/galleries/projects/selling_designer_fabric_with_wordpress.js
@@ -10,6 +10,16 @@ content.push(
     title: "Brand & Website build",
     info: `It was interesting to develop this brand over time from scratch. I worked with an artist whose sketches I would scan, trace and edit into digital vector artwork. Leather and fabric can also be very technical products to market, so it was interesting to see how <a href="https://en-gb.wordpress.org/">Wordpress</a>  could adapt to the needs of the business.`,
   })
+  // create.content({
+  //   src: trailer("alanabananas"),
+  //   title: "VJ Set",
+  //   info: ``,
+  //   height: 110,
+  //   content: {
+  //     width: 800,
+  //     height: 640,
+  //   },
+  // })
 );
 
 export default content;
