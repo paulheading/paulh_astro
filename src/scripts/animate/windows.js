d8b0303d4b76de12e84bb77675ed79e092e77d11
Paul Heading <paulheading@me.com>
2023-12-04 10:51:27 +0000

    thinking about spotify scroll

--- src/scripts/animate/windows.js
+++ src/scripts/animate/windows.js
@@ -1,49 +1,68 @@
-import { gsap } from 'gsap'
-import { Draggable } from 'gsap/dist/Draggable'
-import $ from '~scripts/selectors'
-import { length } from '~data/timings'
+import { gsap } from "gsap";
+import { Draggable } from "gsap/dist/Draggable";
+import $ from "~scripts/selectors";
+import { length } from "~data/timings";
 
-gsap.registerPlugin(Draggable)
+gsap.registerPlugin(Draggable);
 
-let duration = length * 0.05
+let duration = length * 0.05;
 
-function setupDraggables() {
-  for (let index = 0; index < $.windows.length; index++) {
-    const window = $.windows[index]
-    Draggable.create(window)
-  }
+function forEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
 
-  for (let index = 0; index < $.folders.length; index++) {
-    const folder = $.folders[index]
-    const parent = folder.parentNode
-    const subfolder = parent.querySelector('.window')
-
-    if (subfolder) Draggable.create(subfolder)
+    callback(element, index);
   }
 }
 
-const closeState = (target) => target.setAttribute('data-state', 'closed')
+function draggableSubfolder(folder) {
+  const parent = folder.parentNode;
 
-const openState = (target) => target.setAttribute('data-state', 'open')
+  const subfolder = parent.querySelector(".window");
+
+  if (subfolder) Draggable.create(subfolder);
+}
+
+function draggableWindow(window) {
+  // let isSpotify = window.getAttribute("data-group") == "spotify";
+
+  // if (isSpotify) {
+  //   let target = window.querySelector("header");
+
+  //   return Draggable.create(target);
+  // }
+
+  return Draggable.create(window);
+}
+
+function setupDraggables() {
+  forEach($.windows, draggableWindow);
+
+  forEach($.folders, draggableSubfolder);
+}
+
+const closeState = (target) => target.setAttribute("data-state", "closed");
+
+const openState = (target) => target.setAttribute("data-state", "open");
 
 function animateClose(window) {
-  const tl = gsap.timeline()
+  const tl = gsap.timeline();
 
-  const onComplete = () => closeState(window)
+  const onComplete = () => closeState(window);
 
-  tl.to(window, { opacity: 0, zIndex: 1, scale: 0.5, duration, onComplete })
+  tl.to(window, { opacity: 0, zIndex: 1, scale: 0.5, duration, onComplete });
 
-  return tl
+  return tl;
 }
 
 function animateOpen(window) {
-  const tl = gsap.timeline()
+  const tl = gsap.timeline();
 
-  const onStart = () => openState(window)
+  const onStart = () => openState(window);
 
-  tl.to(window, { opacity: 1, zIndex: 2, scale: 1, duration, onStart })
+  tl.to(window, { opacity: 1, zIndex: 2, scale: 1, duration, onStart });
 
-  return tl
+  return tl;
 }
 
-export { setupDraggables, animateClose, animateOpen, closeState, openState }
+export { setupDraggables, animateClose, animateOpen, closeState, openState };
