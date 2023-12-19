5067ab3fc857a3b30c485246ee92fd04bbbb7305
Paul Heading <paulheading@me.com>
2023-12-19 23:38:40 +0000

    search result pagination

--- src/scripts/components/searchbar.js
+++ src/scripts/components/searchbar.js
@@ -18,6 +18,14 @@ let form = submit.parentElement;
 
 let clear = form.nextSibling;
 
+let articlesPerRow = 3;
+
+let seeMore = document.querySelector("#see-more");
+
+let wrapMore = seeMore.closest(".wrap-more");
+
+let seeMoreCount = seeMore.querySelector(".count");
+
 /**
  * @name createEmptyRow
  * @param {HTMLElement} row Existing HTML article row markup with content
@@ -80,13 +88,13 @@ function collectResults(results) {
 
 /**
  * @name printResults
- * @param {*} results
+ * @param {Array<HTMLElement>} results
  */
 
 function printResults(results) {
   results = collectResults(results);
 
-  // let test = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+  let test = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 
   let rows = document.querySelectorAll(".page-row[data=articles]");
 
@@ -97,9 +105,15 @@ function printResults(results) {
   let output = [];
 
   function assignRow(result, index) {
-    let newRow = index % 3 == 0;
+    let newRow = index % articlesPerRow == 0;
 
-    if (newRow) output.push(empty.cloneNode(true));
+    if (newRow) {
+      let clone = empty.cloneNode(true);
+
+      if (index > 0) clone.style.display = "none";
+
+      output.push(clone);
+    }
 
     let lastRow = output[output.length - 1];
 
@@ -108,7 +122,14 @@ function printResults(results) {
     lastContent.append(result);
   }
 
-  results.forEach(assignRow);
+  test.forEach(assignRow);
+
+  let articlesRemaining = test.length - articlesPerRow;
+
+  if (articlesRemaining > 0) {
+    wrapMore.removeAttribute("style", "display");
+    seeMoreCount.innerHTML = `[${articlesRemaining}]`;
+  }
 
   let container = document.querySelector(".container[data=search]");
 
@@ -127,7 +148,7 @@ function searchArticles(event) {
 
   let input = target.previousSibling;
 
-  if (input.value == "") return;
+  if (input.value == "") return clearSearchResults(clear);
 
   clear.removeAttribute("style", "display");
 
@@ -136,9 +157,7 @@ function searchArticles(event) {
   if (results.length) printResults(results);
 }
 
-function clearSearchResults(event) {
-  let { target } = event;
-
+function clearSearchResults(target) {
   let input = submit.previousSibling;
 
   let rows = document.querySelectorAll(".page-row");
@@ -158,6 +177,47 @@ function clearSearchResults(event) {
   target.style.display = "none";
 }
 
+/**
+ * @name decreaseMoreCount
+ * @description Decrease the number of articles displayed within the see more button
+ */
+
+function decreaseMoreCount() {
+  let currentCount = seeMoreCount.innerHTML;
+
+  currentCount = Number(currentCount.slice(1, -1));
+
+  currentCount = currentCount - articlesPerRow;
+
+  seeMoreCount.innerHTML = `[${currentCount}]`;
+
+  if (currentCount <= 0) seeMore.style.display = "none";
+}
+
+/**
+ * @name loadNewRow
+ * @description Display the next remaining row
+ */
+
+function loadNewRow() {
+  let rows = document.querySelectorAll(".page-row[data=search-results]");
+
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index];
+
+    const hidden = row.style.display == "none";
+
+    if (hidden) {
+      row.removeAttribute("style", "display");
+      break;
+    }
+  }
+
+  decreaseMoreCount();
+}
+
 submit.addEventListener("click", searchArticles);
 
-clear.addEventListener("click", clearSearchResults);
+clear.addEventListener("click", ({ target }) => clearSearchResults(target));
+
+seeMore.addEventListener("click", loadNewRow);
