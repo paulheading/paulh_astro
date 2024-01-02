067f6e0aaff77f76176fbbe7da6ddb2e0e484946
Paul Heading <paulheading@me.com>
2024-01-02 18:48:16 +0000

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
+  site: "https://paulh.biz",
   images: {
     domains: ["mosaic.scdn.co", "i.scdn.co", "images-ak.spotifycdn.com"],
   },
   scopedStyleStrategy: "class",
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
