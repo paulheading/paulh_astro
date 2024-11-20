690ef1f5c13fa9abb7b9ddbdead9ea283d11f4a7
Paul Heading <paulheading@me.com>
2024-11-20 12:42:15 +0000

    data tidy up

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,17 +1,13 @@
-const messages = {};
-
-messages.subject = {
-  valueMissing: "Please add an email subject.",
+export default {
+  subject: {
+    valueMissing: "Please add an email subject.",
+  },
+  from: {
+    valueMissing: `Please add your email address.`,
+    typeMismatch: `This doesn't look like an email address.`,
+    tooShort: `Just a few more letters please.`,
+  },
+  message: {
+    valueMissing: `Don't forget your message!`,
+  },
 };
-
-messages.from = {
-  valueMissing: `Please add your email address.`,
-  typeMismatch: `This doesn't look like an email address.`,
-  tooShort: `Just a few more letters please.`,
-};
-
-messages.message = {
-  valueMissing: `Don't forget your message!`,
-};
-
-export default messages;
